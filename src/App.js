c3ca7a0b7b76891dfe902ab45d5fdcf11a395daa
kimdoeun <kdeun1@ex-em.com>
2023-07-01 22:43:34 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="ttt-viewport">
+      <div className="ttt-board">보드영역</div>
+      <div className="ttt-info">인포</div>
+    </div>
+  );
+}
+
+export default App;
